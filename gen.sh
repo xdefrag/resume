b32deb0ec5bf18b02f9158202242447c7419b245
Stanislaw Karkavin <me@xdefrag.dev>
2019-07-31 18:16:30 +0300

    Initial commit

---
+++ gen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+emacsclient --eval "(let ((file \"$(pwd)/resume.org\"))
+    (with-current-buffer (find-file-noselect file)
+    (progn
+        (org-latex-export-to-pdf)
+	(let ((org-export-with-drawers t))
+        (org-md-export-to-markdown)))))"
